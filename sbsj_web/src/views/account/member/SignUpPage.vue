42c067e5800bfa82624019375d421a73ae9a8e91
KMinHong <acute7939@gmail.com>
2023-03-21 21:43:47 +0900

    feat: [김민홍] 사용자가 사이트에 회원가입을 하기 위한 페이지 [HEALTH-FOOD-WEB-UI-1]
    사용자가 사이트에 회원가입을 하기 위한 페이지 [HEALTH-FOOD-WEB-UI-1]

---
+++ sbsj_web/src/views/account/member/SignUpPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <sign-up-form @submit="onSubmit"/>
+  </v-container>
+</template>
+
+<script>
+
+
+import axios from "axios";
+import SignUpForm from "@/components/member/SignUpForm.vue";
+
+export default {
+  name: "SignUpPage",
+  components: {
+    SignUpForm
+  },
+  methods: {
+    onSubmit (payload) {
+      const { membername, memberId, password, email, birthday, phoneNumber , usePass } = payload;
+
+      axios.post("http://localhost:7777/member/sign-up", {
+        membername, memberId, password, email, birthday, phoneNumber , usePass
+      })
+          .then((res) => {
+            alert("회원 가입 완료!" + res)
+            this.$router.push("/sign-in")
+          })
+          .catch((res) => {
+            alert(res.response.data.message)
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
