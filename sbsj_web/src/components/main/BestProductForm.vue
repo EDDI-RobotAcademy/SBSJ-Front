45835549fc1183017dd6b9162bd75fb5aa4d17b0
vceqiv09 <vceqiv@gmail.com>
2023-03-21 21:08:54 +0900

    style: [윤영현] 보여지는 상품 개수 수정, 이미지 수정 [HEALTH-FOOD-WEB-UI-12]

--- sbsj_web/src/components/main/BestProductForm.vue
+++ sbsj_web/src/components/main/BestProductForm.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row class="mb-5">
         <h3>베스트 상품</h3>
-        <v-col v-for="image in images" :key="image" cols="3">
+        <v-col v-for="image in images" :key="image" cols="4">
             <div>
                 <v-img :src="image" aspect-ratio="1" class="grey lighten-2"/>
             </div>
@@ -23,13 +23,9 @@ export default {
     data() {
         return {
             images: [
-                require('@/assets/uploadImgs/gingerTea.jpg'),
-                require('@/assets/uploadImgs/img3.jpg'),
-                require('@/assets/uploadImgs/pumpkinTea.jpg'),
-                require('@/assets/uploadImgs/jujubeTea.jpg'),
-                require('@/assets/uploadImgs/citronTea.jpg'),
                 require('@/assets/uploadImgs/img1.jpg'),
-                require('@/assets/uploadImgs/redginsengTea.jpg'),
+                require('@/assets/uploadImgs/img2.jpg'),
+                require('@/assets/uploadImgs/img3.jpg'),
             ]
         }
     }
