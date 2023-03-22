cd4c923ef93b3979094497fe1a794a72165dd4f1
namgunho <ab5265@naver.com>
2023-03-22 18:02:55 +0900

    feat: [남건호] 메인헤더 역할을 하는 네비게이션바 구현 [HEALTH-FOOD-WEB-UI-5]

--- sbsj_web/src/views/layoutForm/Header.vue
+++ sbsj_web/src/views/layoutForm/Header.vue
@@ -1,71 +1,53 @@
 <template>
   <nav>
-    <v-app-bar color="dark" class="flex-grow-0" app dark>
-      <v-app-bar-nav-icon @click="navigation_drawer = !navigation_drawer"/>
-      <v-img class="mx-2" src="@/assets/logo.png"
-              max-height="40" max-width="40" contain/>
-      <v-toolbar-title class="text-uppercase text--darken-4">
-        <span>PYE</span>
+  <v-app-bar color="dark" class="flex-grow-0" app dark>
+    <v-app-bar-nav-icon v-on:click="activeSidebar"/>
+    <v-img class="mx-2" src="@/assets/logo.png"
+            max-height="40" max-width="40" contain/>
+      <v-toolbar-title class="text--darken-4">
+          <span>Pick Your Energy</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn v-if="isTrue == true" text @click="clickToggle">
-        <span>응답하라</span>
-        <v-icon right>mdi-test-tube</v-icon>
+      <v-text-field class="green--text" v-model="search" append-icon="mdi-magnify" label="placeholder" single-line hide-details></v-text-field>
+      <v-btn v-if="isAuthenticated == true" text color="grey" v-on:click="resign">
+          <span>회원 탈퇴</span>
+          <v-icon right>mdi-login</v-icon>
       </v-btn>
-      <v-btn v-else text @click="clickToggle">
-        <span>응답하라구!!</span>
-        <v-icon right>mdi-history</v-icon>
+      <v-btn text color="grey" onclick="location.href='http://localhost:8080/sign-up'">
+          <span>Sign Up</span>
+          <v-icon right>mdi-account-plus-outline</v-icon>
       </v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer app v-model="navigation_drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">EDDI</v-list-item-title>
-          <v-list-item-subtitle>Functions</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list nav dense>
-        <v-list-item v-for="link in links" :key="link.name" router :to="link.route">
-          <v-list-item-action>
-            <v-icon left>
-              {{ link.icon }}
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ link.text }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </nav>
+      <v-btn v-if="isAuthenticated == false" text color="grey" onclick="location.href='http://localhost:8080/sign-in'">
+          <span>Sign In</span>
+          <v-icon right>mdi-login</v-icon>
+      </v-btn>
+      <v-btn v-else text color="grey" v-on:click="logout">
+          <span>Sign Out</span>
+          <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+  </v-app-bar>    
+</nav>
 </template>
 
 <script>
 export default {
-    name: "Header",
-    data () {
-        return {
-            isTrue: false,
-            navigation_drawer: false, // true로 지정하면 기본적으로 메뉴가 튀어나와있다.
-            links: [
-                { icon: 'mdi-home', text: 'Home', name: 'home', route: '/' },
-                { icon: 'mdi-share-variant-outline', text: 'Sharing', 
-                    name: 'JpaBoardListPage', route: '/board-list-page' },
-            ]
-        }
-    },
-    methods: {
-        clickToggle () {
-        this.isTrue = !this.isTrue
-        }
-    }
-}
+
+
+</style>          //
+      },
+      logout () {
+          //
+      }
+  },
+  // computed: {
+  //     …mapState(["isAuthenticated"]),
+  // },
+  // mounted() {
+  //     if (localStorage.getItem("userInfo")) {
+  //         this.$store.state.isAuthenticated = true;
+  //     } else {
+  //         this.$store.state.isAuthenticated = false;
+  //     }
+  // }
+  }
 </script>
-
-<style>
-
-</style>
