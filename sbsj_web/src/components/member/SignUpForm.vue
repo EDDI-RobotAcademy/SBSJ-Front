16a3b54975111f3a495426d2242697125bf2b645
KMinHong <acute7939@gmail.com>
2023-03-27 18:01:50 +0900

    style: [김민홍] 배경색, 버튼 스타일 변경 [HEALTH-FOOD-WEB-UI-1]
    style: [김민홍] 배경색, 버튼 스타일 변경 [HEALTH-FOOD-WEB-UI-1]

--- sbsj_web/src/components/member/SignUpForm.vue
+++ sbsj_web/src/components/member/SignUpForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grey lighten-5" style="font-family: Arial">
+  <div class="" style="font-family: Arial">
     <v-row justify="center">
       <v-col cols="auto" style="padding-bottom: 90px">
         <router-link to="/">
