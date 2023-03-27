ecda1dc30db397ef84e6135bee844a192d4568fc
vceqiv09 <vceqiv@gmail.com>
2023-03-27 21:30:39 +0900

    style: [윤영현] 메인 페이지 상단에 들어가는 광고, 이벤트, 프로모션에 대한 Carousel UI [HEALTH-FOOD-WEB-UI-7]

--- sbsj_web/src/components/main/CarouselForm.vue
+++ sbsj_web/src/components/main/CarouselForm.vue
@@ -1,37 +1,79 @@
 <template>
-    <v-carousel cycle height="400" hide-delimiter-background show-arrows="hover">
-        <v-carousel-item v-for="(picture, idx) in pictures" :key="idx">
-            <v-sheet :height="height" tile>
-                <div class="d-flex fill-height justify-center align-center">
-                    <div class="text-h2">
-                        <v-img :src="picture.img" :height="height" contain />
-                    </div>
-                </div>
-            </v-sheet>
-        </v-carousel-item>
-    </v-carousel>
+	<div id="mainTopCarousel" class="carousel slide" align="center" data-bs-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<div class="main-top-promotion">
+					<video class="main-top-promotion-video" autoplay="autoplay" loop="loop" muted="muted">
+            			<source src="@/assets/carousel/video1.mp4" type="video/mp4">
+					</video>
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-sm-12 col-md-6 text-md-start"/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="main-top-promotion">
+					<video class="main-top-promotion-video" autoplay="autoplay" loop="loop" muted="muted">
+            			<source src="@/assets/carousel/video2.mp4" type="video/mp4">
+					</video>
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-sm-12 col-md-6 text-md-start"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#mainTopCarousel" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#mainTopCarousel" data-bs-slide="next" onclick="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
 </template>
 
 <script>
 
 export default {
     name: "CarouselForm",
-    data() {
-        return {
-            model: 0,
-            pictures: [
-                { img: require("@/assets/uploadImgs/img1.jpg") },
-                { img: require("@/assets/uploadImgs/img2.jpg") },
-                { img: require("@/assets/uploadImgs/img3.jpg") },
-            ],
-            cycle: true,
-            interval: 3000,  // ms 단위
-        }
-    }
+	methods: {
+		
+	}
 }
 
 </script>
 
 <style>
-
+	.main-top-promotion {
+		position: relative;
+		width: 99%;
+		height: 500px;
+	}
+	.main-top-promotion-video {
+	height: 100%;
+	width: 100%;
+	margin: 0px;
+	padding: 0px;
+	position: absolute;
+	left: 0;
+	top: 0;
+	object-fit: cover;
+	}
+	.main-top-promotion>.container {
+		position: relative;
+		
+	}
+	.carousel-control-next,
+	.carousel-control-prev {
+		width: 7% !important;
+	}
 </style>
--- sbsj_web/src/components/main/MainForm.vue
+++ sbsj_web/src/components/main/MainForm.vue
@@ -1,10 +1,6 @@
 <template>
-  <v-container>
-    <div id="home">
-      <div class="row">
-        <div class="col-md elevation-1 ma-3 pa-3">
-          <carousel-form/>
-        </div>
+  <div id="home" class="mt-2">
+    <carousel-form/>
       </div>
       <div class="row">
         <div class="col-md elevation-1 ma-3 pa-3">
@@ -14,12 +10,12 @@
           <recommend-product-form/>
         </div>
       </div>
-      <div class="col-md ma-5 pa-5"></div>
     </div>
-  </v-container>
+  </div>
 </template>
 
 <script>
+
 import CarouselForm from "@/components/main/CarouselForm.vue"
 import BestProductForm from '@/components/main/BestProductForm.vue';
 import RecommendProductForm from '@/components/main/RecommendProductForm.vue';
