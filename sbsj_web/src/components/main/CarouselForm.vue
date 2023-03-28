4309ab8a5a71cdd49363139db91d824057a4d0ca
vceqiv09 <vceqiv@gmail.com>
2023-03-28 12:50:52 +0900

    fix: [윤영현 메인 페이지 상단에 들어가는 Carousel 옆으로 안 넘어가는 버그 수정 [HEALTH-FOOD-WEB-UI-7]

--- sbsj_web/src/components/main/CarouselForm.vue
+++ sbsj_web/src/components/main/CarouselForm.vue
@@ -1,40 +1,30 @@
 <template>
 	<div id="mainTopCarousel" class="carousel slide" align="center" data-bs-ride="carousel">
 		<div class="carousel-inner">
-			<div class="carousel-item active">
+			<div class="carousel-item active carousel">
 				<div class="main-top-promotion">
 					<video class="main-top-promotion-video" autoplay="autoplay" loop="loop" muted="muted">
             			<source src="@/assets/carousel/video1.mp4" type="video/mp4">
 					</video>
-					<div class="container">
-						<div class="row align-items-center">
-							<div class="col-sm-12 col-md-6 text-md-start"/>
-						</div>
-					</div>
 				</div>
 			</div>
-			<div class="carousel-item">
+			<div class="carousel-item carousel">
 				<div class="main-top-promotion">
 					<video class="main-top-promotion-video" autoplay="autoplay" loop="loop" muted="muted">
             			<source src="@/assets/carousel/video2.mp4" type="video/mp4">
 					</video>
-					<div class="container">
-						<div class="row align-items-center">
-							<div class="col-sm-12 col-md-6 text-md-start"/>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
 		<div class="carousel-indicators">
-			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="0" class="indicator active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="1" class="indicator" aria-label="Slide 2"></button>
 		</div>
-		<button class="carousel-control-prev" type="button" data-bs-target="#mainTopCarousel" data-bs-slide="prev">
+		<button class="carousel-control-prev" type="button" data-bs-target="#mainTopCarousel" data-bs-slide="prev" @click="pass">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="visually-hidden">Previous</span>
 		</button>
-		<button class="carousel-control-next" type="button" data-bs-target="#mainTopCarousel" data-bs-slide="next" onclick="next">
+		<button class="carousel-control-next" type="button" data-bs-target="#mainTopCarousel" data-bs-slide="next" @click="pass">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="visually-hidden">Next</span>
 		</button>
@@ -46,7 +36,25 @@
 export default {
     name: "CarouselForm",
 	methods: {
-		
+		pass() {
+			let carousel1 = document.getElementsByClassName("carousel-item")[0];
+			let carousel2 = document.getElementsByClassName("carousel-item")[1];
+
+			let indicator1 = document.getElementsByClassName("indicator")[0];
+			let indicator2 = document.getElementsByClassName("indicator")[1];
+
+			if(carousel1.classList.contains("active")) {
+				carousel1.classList.remove("active");
+				carousel2.classList.add("active");
+				indicator1.classList.remove("active");
+				indicator2.classList.add("active");
+			} else {
+				carousel1.classList.add("active");
+				carousel2.classList.remove("active");
+				indicator1.classList.add("active");
+				indicator2.classList.remove("active");
+			}
+		}
 	}
 }
 
@@ -56,17 +64,17 @@ export default {
 	.main-top-promotion {
 		position: relative;
 		width: 99%;
-		height: 500px;
+		height: 600px;
 	}
 	.main-top-promotion-video {
-	height: 100%;
-	width: 100%;
-	margin: 0px;
-	padding: 0px;
-	position: absolute;
-	left: 0;
-	top: 0;
-	object-fit: cover;
+		height: 100%;
+		width: 100%;
+		margin: 0px;
+		padding: 0px;
+		position: absolute;
+		left: 0;
+		top: 0;
+		object-fit: cover;
 	}
 	.main-top-promotion>.container {
 		position: relative;
