0bb58401563bbcaa451a8fd96ac0b34c7e7e6f39
vceqiv09 <vceqiv@gmail.com>
2023-03-31 15:39:14 +0900

    rename: [윤영현] 비밀번호 찾기 페이지 이름 변경 [HEALTH-FOOD-WEB-UI-4]

---
+++ sbsj_web/src/views/account/member/SearchPasswordPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <search-password-form></search-password-form>
+  </div>
+</template>
+
+<script>
+
+import SearchPasswordForm from "@/components/member/SearchPasswordForm.vue";
+
+export default {
+  name: "SearchPasswordPage",
+  components: {
+    SearchPasswordForm
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
