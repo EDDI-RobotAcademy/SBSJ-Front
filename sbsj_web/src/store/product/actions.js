527bfb8128215901b0979d9037c9aa686c0cc69c
namgunho <ab5265@naver.com>
2023-04-04 20:27:03 +0900

    feat: [남건호] 상품 리스트 게시판 Backend 데이터와 연동 작업[HEALTH-FOOD-WEB-UI-39]
    상품 리스트 게시판 Backend 데이터와 연동 작업[HEALTH-FOOD-WEB-UI-39]

---
+++ sbsj_web/src/store/product/actions.js
@@ -0,0 +1,16 @@
+import {
+    REQUEST_PRODUCT_LIST_TO_SPRING,
+} from './mutation-types'
+
+import axiosInst from '@/utility/axiosObject'
+
+export default {
+    async requestProductListToSpring({commit}) {
+        return await axiosInst.get('/product/default')
+            .then((res) => {
+                commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data);
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+}
--- sbsj_web/src/views/product/ProductPage.vue
+++ sbsj_web/src/views/product/ProductPage.vue
@@ -29,35 +29,24 @@ import JpaProductList from '@/components/product/JpaProductList.vue';
 import ProductSearchFilter from '@/components/product/JpaSearchFilter.vue';
 import ProductCategoryHeader from '@/components/product/ProductCategoryHeader.vue';
 
-// temp
-import axios from 'axios';
-  
+const productModule = 'productModule'
   export default {
     name: "ProductPage",
     components: {JpaProductList, ProductSearchFilter, ProductCategoryHeader},
-    data() {
-        return {
-            products: [],
-        }
+
+    computed: {
+        ...mapState(productModule, [
+            'products'
+            ])
     },
-    // computed: {
-    //     ...mapState([
-    //         'products'
-    //         ]),
-    // },
     mounted () {
-        // this.requestProductListtoSpring()
-        console.log("mounted()");
-        axios.get('http://localhost:7777/product/default')
-            .then((res) => this.products = res.data)
-            .catch((res) => {
-              alert(res.response.data.message);
-            });
-    },
+        this.requestProductListToSpring()
+        },
+
     methods: {
-        // ...mapActions([
-        //     'requestProductListtoSpring'
-        // ])
+        ...mapActions(productModule, [
+            'requestProductListToSpring'
+        ])
     }
   }
   
