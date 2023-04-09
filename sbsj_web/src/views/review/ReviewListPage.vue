85d5f47522da52bb01b14fd2752956dac8338f99
KMinHong <acute7939@gmail.com>
2023-04-09 22:34:54 +0900

    feat: [김민홍] 상품상세 페이지에서 사용자가 리뷰를 작성하고 작성한 목록이 보여지는 컴포넌트 페이지 [HEALTH-FOOD-WEB-UI-38]

---
+++ sbsj_web/src/views/review/ReviewListPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <review-list-form/>
+</template>
+  
+  <script>
+  
+  import ReviewListForm from '@/components/review/ReviewListForm.vue';
+  
+  
+  export default {
+      name: "ReviewListPage",
+      components: {
+          ReviewListForm
+   
+    },
+  }
+  </script>
+  
+  <style>
+  
+  </style>
