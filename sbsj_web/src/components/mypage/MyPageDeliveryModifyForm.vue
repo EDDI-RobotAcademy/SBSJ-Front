237b061beb484359b416539f8932a0825dfc6e9f
vceqiv09 <vceqiv@gmail.com>
2023-04-10 02:58:28 +0900

    feat: [윤영현] 마이페이지의 ‘배송지관리’ 탭에 있는 수정 버튼을 클릭 하면 뜨는 다이얼로그 UI [HEALTH-FOOD-WEB-UI-53]

---
+++ sbsj_web/src/components/mypage/MyPageDeliveryModifyForm.vue
@@ -0,0 +1,155 @@
+<template>
+    <v-layout>
+        <v-dialog v-model="dialog" persistent max-width="800" max-height="1000">
+            <template v-slot:activator="{on}">
+                <v-btn class="w-10 me-2" v-on="on">수정</v-btn>
+            </template>
+
+            <v-form @submit.prevent="onSubmit" ref="form">
+                <v-card height="850" class="rounded-xl">
+                    <div class="d-flex justify-end">
+                        <v-icon class="mt-6 me-6 mb-0" @click="btnCancel()">mdi-close</v-icon>
+                    </div>
+                    <v-card-title class="headline d-flex justify-center" style="padding: 15px 0 25px 0;">
+                        배송지 수정
+                    </v-card-title>
+                    <br>
+                    <v-card-text style="padding: 0 100px 0 100px">
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.addressName" label="배송지명 ex) 우리집"
+                                :rules="addressName_rule" color="green" required/>
+                        </div>
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.addressType" label="배송지 분류"
+                                :rules="addressType_rule" color="green" required/>
+                        </div>
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.recipientName" label="수령인 이름"
+                                :rules="recipientName_rule" required color="green"/>
+                        </div>
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.phoneNumber" label="전화번호 ex) 010-1234-5678"
+                                :rules="phoneNumber_rule" required color="green"/>
+                        </div>
+
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.city" label="도시" :disabled="true" required/>
+                        </div>
+
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.street" label="기본 주소" :disabled="true" required/>
+                        </div>
+
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.addressDetail" label="상세 주소" color="green" required/>
+                        </div>
+                        
+                        <div class="d-flex">
+                            <v-text-field v-model="localDelivery.zipcode" label="우편번호" :disabled="true" required/>
+                            
+                            <v-btn text large outlined style="font-size: 13px"
+                                class="mt-3 ml-5" color="blue lighten-1"
+                                @click="callDaumAddressApi">
+                                주소 확인
+                            </v-btn>
+                        </div>
+                        <div class="form-check">
+                            <input v-if="localDelivery.defaultAddress === ''" class="form-check-input" type="checkbox" id="defaultAddress">
+                            <input v-else class="form-check-input" type="checkbox" id="defaultAddress" checked>
+                            <label class="form-check-label ms-1" for="defaultAddress" >기본 배송지로 설정</label>
+                        </div>
+                    </v-card-text>
+                    <div class="d-flex justify-center">
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn type="submit" width="610px" x-large rounded
+                                    class="mt-6" color="#c7d6cd">
+                                배송지 등록
+                            </v-btn>
+                        </v-card-actions>
+                    </div>
+                </v-card>
+            </v-form>
+        </v-dialog>
+    </v-layout>
+</template>
+
+<script>
+
+export default {
+    name: "MyPageDeliveryModifyForm",
+    props: {
+        delivery: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            dialog: false,
+            streetPass: false,
+            localDelivery: {},
+            
+            addressName_rule: [
+                v => !!v || '배송지명을 입력 해주세요.',
+            ],
+            addressType_rule: [
+                v => !!v || '배송지 분류를? 입력 해주세요.',
+            ],
+            recipientName_rule: [
+                v => !!v || '수령인 이름을 입력 해주세요.',
+            ],
+            phoneNumber_rule: [
+                v => !!v || '전화번호를 입력 해주세요.',
+                v => {
+                    const replaceV = v.replace(/(\s*)/g, '')
+                    const pattern = /^(010|011|016|017|018|019)-[0-9]{3,4}-[0-9]{4}$/
+                    return pattern.test(replaceV) || '010-1234-5678 형식의 번호를 입력해주세요.'
+                }
+            ]
+        }
+    },
+    created() {
+        this.localDelivery = this.delivery;
+    },
+        btnCancel() {
+            this.dialog = false;
+        },
+        callDaumAddressApi () {
+            new window.daum.Postcode({
+                oncomplete: (data) => {
+                let fullRoadAddr = data.roadAddress;
+                let extraRoadAddr = '';
+
+                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+                    extraRoadAddr += data.bname;
+                }
+
+                if (data.buildingName !== '' && data.apartment === 'Y') {
+                    extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+
+                if (extraRoadAddr !== '') {
+                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+                }
+
+                if (fullRoadAddr !== '') {
+                    fullRoadAddr += extraRoadAddr;
+                }
+
+                this.localDelivery.city = data.sido;
+                this.localDelivery.zipcode = data.zonecode;
+                this.localDelivery.street = data.sigungu + ' ' + fullRoadAddr;
+
+                this.streetPass = true
+                }
+            }).open()
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
