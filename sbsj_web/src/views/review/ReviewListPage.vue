3f2125ac3ef844301ab0d73e7376897221e0e75d
KMinHong <acute7939@gmail.com>
2023-04-16 19:48:58 +0900

    fix:[김민홍] 리뷰 리스트페이지 에러 수정

--- sbsj_web/src/views/review/ReviewListPage.vue
+++ sbsj_web/src/views/review/ReviewListPage.vue
@@ -1,21 +1,37 @@
 <template>
-    <review-list-form/>
+  <review-list-form :product="product" />
 </template>
-  
-  <script>
-  
-  import ReviewListForm from '@/components/review/ReviewListForm.vue';
-  
-  
-  export default {
-      name: "ReviewListPage",
-      components: {
-          ReviewListForm
-   
-    },
+
+<script>
+
+import ReviewListForm from '@/components/review/ReviewListForm.vue';
+import { mapState } from 'vuex';
+
+export default {
+    name: "ReviewListPage",
+    components: {
+        ReviewListForm
+ 
+  },
+  data(){
+  return{
+    isCompeleteDelete: false
   }
-  </script>
-  
-  <style>
-  
-  </style>
+},
+props: {
+  product: {
+    type: Object,
+    required: true
+  }
+},
+computed: {
+  ...mapState([
+    'product', 'isAuthenticated'
+  ])
+},
+}
+</script>
+
+<style>
+
+</style>
