f733a35592219205501b8e791c6f2037ac2f89d5
vceqiv09 <vceqiv@gmail.com>
2023-04-16 17:54:16 +0900

    rename: [윤영현] 상품 상세보기 params 에 productId type Number 에서 String 으로 수정 [HEALTH-FOOD-WEB-UI-19]

--- sbsj_web/src/views/product/DetailProductPage.vue
+++ sbsj_web/src/views/product/DetailProductPage.vue
@@ -9,16 +9,16 @@
 import DetailProductForm from '@/components/product/DetailProductForm.vue'
 import { mapActions, mapState } from 'vuex';
 
-const productModule = 'productModule'
+const productModule = 'productModule';
 
 export default {
   components: { DetailProductForm },
   name: "DetailProductPage",
   props: {
-      productId: {
-        type: Number,
-        required: true,
-      }
+    productId: {
+      type: String,
+      required: true,
+    }
   },
   methods: {
     ...mapActions(productModule, ['requestProductToSpring']),
