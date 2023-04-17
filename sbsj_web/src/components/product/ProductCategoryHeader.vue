185fe9e32d3d53ceb1333b40ebeec614a821e339
namgunho <ab5265@naver.com>
2023-04-17 20:03:34 +0900

    feat: [남건호] 사용자가 상품리스트 페이지에서 필터기능, 정렬기능 컴포넌트 클릭시 필터링 및 정렬 관련 함수[HEALTH-FOOD-WEB-UI-80]
    사용자가 상품리스트 페이지에서 필터기능, 정렬기능 컴포넌트 클릭시 필터링 및 정렬 관련 함수[HEALTH-FOOD-WEB-UI-80]

--- sbsj_web/src/components/product/ProductCategoryHeader.vue
+++ sbsj_web/src/components/product/ProductCategoryHeader.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="product-main-category-header">
-      <h3 class="product-list-title">Selected Category Title here</h3>
+      <h3 class="product-list-title">{{ selectedTitle }}</h3>
       <div class="search-utilities">
         <div class="search-sorting">
-          <div class="search-sorting-order">Search Sorting here</div>
+          <div class="search-sorting-order">
+            <v-btn class="sort-btn" v-on:click="priceDesc">높은가격순</v-btn>
+            <v-btn class="sort-btn" v-on:click="priceAsc">낮은가격순</v-btn>
+            <v-btn class="sort-btn" v-on:click="wishCountDesc">찜 순</v-btn>
+          </div>
           <div class="search-sorting-list">Search Sorting List here</div>
         </div>
       </div>
@@ -11,6 +15,9 @@
   </template>
 
   <style scoped>
+    .sort-btn {
+      display: inline-block;
+    }
     .search-utilities {
       max-width: 100%;
       height: 32px;
@@ -37,7 +44,27 @@
   
   export default {
     name: 'ProductCategoryHeader',
+    props: {
+      selectedTitle: {
+        type: String
+      }
+    },
+    methods: {
+      priceDesc() {
+        console.log("priceDesc() in Component")
+        this.$emit('priceDesc')
+      },
+      priceAsc() {
+        console.log("priceAsc() in Component")
+        this.$emit('priceAsc')
+      },
+      wishCountDesc() {
+        console.log("wishCountDesc() in Component")
+        this.$emit('wishCountDesc')
+      }
+    }
   }
+    
   
   </script>
   
--- sbsj_web/src/views/product/ProductListPage.vue
+++ sbsj_web/src/views/product/ProductListPage.vue
@@ -8,20 +8,21 @@
                 <div class="product-main">
                     <div class="product-list">
                         <div class="product-main-category-header">
-                            <product-category-header/>
+                            <product-category-header :selected-title="selectedTitle" @priceDesc="priceDesc" @priceAsc="priceAsc" @wishCountDesc="wishCountDesc"/>
                         </div>
                         <div class="clear-fix"></div>
-                        <product-list-form :products="products"/>
+                        <product-list-form :products="showProducts"/>
                         <div class="clear-fix"></div>
                     </div>
                 </div>
                 <div class="product-nav">
-                    <product-list-filter/>
+                    <product-list-filter @filtering="filteringProduct"/>
                 </div>
             </div>
         </div>
     </section>
   </template>
+  
 <script>
 
 import { mapActions, mapState } from 'vuex';
@@ -30,24 +31,76 @@ import ProductListFilter from '@/components/product/ProductListFilter.vue';
 import ProductCategoryHeader from '@/components/product/ProductCategoryHeader.vue';
 
 const productModule = 'productModule'
-
   export default {
     name: "ProductListPage",
     components: {ProductListForm, ProductListFilter, ProductCategoryHeader},
+    data() {
+        return {
+            category: 'TOTAL',
+            temp: [],
+            productTitle: ''
+        }
+    },
+
+    
+    async created() {
+        await this.requestProductListToSpring();
+        this.temp = this.products
+        this.productTitle = this.category
+        console.log(this.productTitle)
+        },
 
     computed: {
-        ...mapState(productModule, [
-            'products'
-            ])
+        showProducts: {
+            get() {
+                // console.log("showProducts() get()")
+                return this.temp
+            },
+            set(value) {
+                // console.log("showProducts() set()")
+                this.temp = value
+            },
+        },
+        selectedTitle: {
+            get() {
+                return this.productTitle
+            },
+            set(value) {
+                this.productTitle = value
+            }
+        },
+        ...mapState(productModule, ['products']),
     },
-    mounted () {
-        this.requestProductListToSpring()
-    },
-
     methods: {
         ...mapActions(productModule, [
             'requestProductListToSpring'
-        ])
+        ]),
+        filteringProduct(productCategory) {
+            // console.log("filteringProduct();")
+            this.category = productCategory
+            console.log(this.category)
+            if(this.category === 'TOTAL') {
+                console.log("total category")
+                this.showProducts = this.products
+            } else {
+                // console.log("specific category")
+                console.log(this.products.filter((product) => product.productOptions.includes(this.category)))
+                this.showProducts = this.products.filter((product) => product.productOptions.includes(this.category))
+            }
+            this.productTitle = this.category
+        },
+        priceDesc() {
+            console.log("princeDesc() in View page")
+            this.temp.sort((a, b) => b.price - a.price)
+        },
+        priceAsc() {
+            console.log("princeAsc() in View page")
+            this.temp.sort((a, b) => a.price - b.price)
+        },
+        wishCountDesc() {
+            console.log("wishCountDesc in View page")
+            this.temp.sort((a, b) => b.wishCount - a.wishCount)
+        }
     }
   }
   
