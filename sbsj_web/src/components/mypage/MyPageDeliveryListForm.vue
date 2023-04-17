09b113b20036359362cb919836ce7cb8c0a4a26b
vceqiv09 <vceqiv@gmail.com>
2023-04-17 18:17:17 +0900

    feat: [윤영현] 배송지 등록 후 페이지 새로고침 [HEALTH-FOOD-WEB-UI-51]

--- sbsj_web/src/components/mypage/MyPageDeliveryListForm.vue
+++ sbsj_web/src/components/mypage/MyPageDeliveryListForm.vue
@@ -64,7 +64,7 @@ export default {
 
         async onSubmit(payload) {
             await this.reqMyPageRegisterDeliveryToSpring(payload);
-            // window.location.reload(true);
+            window.location.reload(true);
         }
     },
 }
