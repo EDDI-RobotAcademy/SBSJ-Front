68170e188a523b3107327943a63007d22174589f
vceqiv09 <vceqiv@gmail.com>
2023-04-17 17:10:28 +0900

    rename: [윤영현] component 이름 수정 [HEALTH-FOOD-WEB-UI-59]

--- sbsj_web/src/views/review/ReviewRegisterPage.vue
+++ sbsj_web/src/views/review/ReviewRegisterPage.vue
@@ -1,26 +1,24 @@
 <template>
-  <ReviewRegisterForm :product="product" />
+  <review-register-form :product="product" />
 </template>
 
 <script>
 
 import ReviewRegisterForm from '@/components/review/ReviewRegisterForm.vue';
 
-
 export default {
-    name: "ReviewRegisterPage",
-    components: {
-        ReviewRegisterForm
- 
+  name: "ReviewRegisterPage",
+  components: {
+      ReviewRegisterForm
   },
   data() {
-  return {
-    product: {
-      productId: '12345',
-      // 기타 필요한 상품 정보
-    },
-  };
-},
+    return {
+      product: {
+        productId: '12345',
+        // 기타 필요한 상품 정보
+      },
+    };
+  },
 }
 </script>
 
