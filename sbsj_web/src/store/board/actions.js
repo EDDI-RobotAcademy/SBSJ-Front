2fb63f77207918a4da0ab6e76dc98a9953ef93f8
zinif0607 <125858642+zinif0607@users.noreply.github.com>
2023-04-23 23:40:57 +0900

    add: [황동희] FreeBoaedReadPage 내용 추가 [HEALTH-FOOD-WEB-UI-91]

--- sbsj_web/src/store/board/actions.js
+++ sbsj_web/src/store/board/actions.js
@@ -2,6 +2,9 @@ import {
     REQUEST_QNA_BOARD_LIST_TO_SPRING,
     REQUEST_QNA_BOARD_TO_SPRING,
     REQUEST_QNA_COMMENT_LIST_FROM_SPRING,
+
+    REQUEST_FREE_BOARD_LIST_TO_SPRING,
+    REQUEST_FREE_BOARD_TO_SPRING,
 } from './mutation-types'
 
 import axiosInst from '@/utility/axiosObject.js'
