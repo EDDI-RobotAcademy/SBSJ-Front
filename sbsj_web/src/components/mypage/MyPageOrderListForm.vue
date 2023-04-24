1915a73f3a415db5a0e2fc21904243db2a293893
Fromredtoviolet <aprilartists@gmail.com>
2023-04-24 19:53:56 +0900

    feat: [서예인] 로그인한 사용자가 마이페이지에서 확인할 수 있는 ‘주문 관리’ 탭 UI [HEALTH-FOOD-WEB-UI-86]

---
+++ sbsj_web/src/components/mypage/MyPageOrderListForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mt-10 mx-5">
+        <div v-if="!completeOrderList || (Array.isArray(completeOrderList) && completeOrderList.length === 0)">
+            <div class="d-flex justify-center">
+                <h1 class="mt-10">아무것도 없음</h1>
+            </div>
+        </div>
+        <div v-else>
+            <v-card v-for="completeOrder in completeOrderList" :key="completeOrder.orderId" class="mb-5 rounded-xl">
+                <v-card-text>
+                    <div class="mb-1">주문번호: {{ completeOrder.orderNo }}</div>
+                    <div class="mb-1">주문일시: {{ completeOrder.orderDate | formatDate }}</div>
+                    <div class="mb-1">
+                        주문상품: {{ completeOrder.orderTotalCount === 1 ? completeOrder.orderItemList[0].productName : `${completeOrder.orderItemList[0].productName} 외 ${completeOrder.orderTotalCount - 1}건` }}
+                    </div>
+                    <div class="mb-1">결제금액: {{ new Intl.NumberFormat().format(completeOrder.amount) }}원</div>
+                </v-card-text>
+                <v-card-actions class="ms-2 pb-5 pt-0">
+                    <my-page-order-read-form :completeOrder="completeOrder"/>
+                </v-card-actions>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "MyPageOrderListForm",
+    
+}
+</script>
+
+<style>
+
+</style>
