52d1b1a24fef9d8dbcd2a7012edd0111b84d3b9f
zinif0607 <125858642+zinif0607@users.noreply.github.com>
2023-04-26 02:48:26 +0900

    feat: [황동희] 작성자가 꿀팁 게시판에서 본인 댓글을 수정할수 있는 UI  [HEALTH-FOOD-WEB-UI-100]

--- sbsj_web/src/components/board/comment/FreeCommentListForm.vue
+++ sbsj_web/src/components/board/comment/FreeCommentListForm.vue
@@ -8,6 +8,10 @@
                 <div class="free-comment">
                 <p style="font-weight:bold;">{{ freeComment.writer }} </p>
                 <p v-show="commentModify !== index">{{ freeComment.comment }}</p>
+                <v-text-field v-model="freeComment.comment" label="댓글 수정" v-show="commentModify === index"></v-text-field>
+                <button v-if="commentModify !== index" @click="startModify(index)">수정 | </button>
+                <button v-if="commentModify === index" @click="saveComment(freeComment)">수정 완료 | </button>
+                <button @click="deleteComment(freeComment)">삭제</button>
                 </div>
             </v-card-text>
         </v-container>
@@ -34,6 +38,18 @@ export default {
     methods: {
         startModify(index) {
             this.commentModify = index;
+        },
+        saveComment(payload) {
+            this.commentModify = null;
+            const { freeCommentId, comment } = payload;
+            return axios.put(`http://localhost:7777/free/read/${freeCommentId}`, 
+                {comment})
+                .then((res) => {
+                    alert("질문 게시글의 댓글 " + freeCommentId + "번 -> " + comment  +"로 수정 성공", res.data)
+                })
+                .catch(() => {
+                    alert("질문 게시글의 댓글 " + freeCommentId + "번 수정 실패")
+                })
         },
     },
 }
