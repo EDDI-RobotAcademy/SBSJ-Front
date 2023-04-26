4a780657e0ff81ade59dd58d1d147465b3e188f1
vceqiv09 <vceqiv@gmail.com>
2023-04-26 19:00:34 +0900

    add: [윤영현 '맞춤 영양제 추천' 과정 중 신체기관 관련 선택 페이지 이름, style 수정 및 localStorage 에 데이터 추가 [HEALTH-FOOD-WEB-UI-13]

---
+++ sbsj_web/src/views/survey/visceraCheckList/BoneCheckPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <bone-check :questions="this.questions"/>
+    </div>
+</template>
+    
+<script>
+
+import BoneCheck from '@/components/survey/visceraCheckList/BoneCheck.vue';
+
+export default {
+    name: "BoneCheckPage",
+    components: { BoneCheck },
+    data() {
+        return {
+            questions: [
+                { question: "발목을 삐끗하거나 뼈가 부러지는 경우가 많다." },
+                { question: "스테로이드 계통의 약물을 3개월 이상 섭취 혹은 투약하고 있다." },
+                { question: "예전 보다 손톱이 약해졌거나 쉽게 부러진다." },
+                { question: "다이어트를 자주 하고, 단일 식품으로 다이어트를 하거나 굶는 경우가 많다." },
+                { question: "보기에 해당사항은 없으나 뼈건강 관리를 하고 싶다." },
+            ]
+        }
+    }
+}
+  </script>
+  
+  <style>
+  </style>
---
+++ sbsj_web/src/views/survey/visceraCheckList/VisceraSurveyPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <viscera-survey-form/>
+  </div>
+</template>
+
+<script>
+
+import VisceraSurveyForm from '@/components/survey/visceraCheckList/VisceraSurveyForm.vue';
+
+export default {
+  name : "VisceraSurveyPage",
+  components: { VisceraSurveyForm },
+}
+
+</script>
+
+<style>
+
+</style>
