9b4f8488aad5f395df4e7ca6be0addb0c9e20461
namgunho <ab5265@naver.com>
2023-04-27 15:43:13 +0900

    add: [남건호] 자유게시판 게시물 페이징처리 [HEALTH-FOOD-WEB-UI-81]
    자유게시판 게시물 페이징처리 [HEALTH-FOOD-WEB-UI-81]

--- src/views/board/free/FreeBoardListPage.vue
+++ src/views/board/free/FreeBoardListPage.vue
@@ -1,28 +1,89 @@
 <template>
   <div>
-    <free-board-list-form :freeBoards="freeBoards"/>
+    <free-board-list-form :freeBoards="showFreeBoards"/>
+    <div class="clear-fix"></div>
+    <paginate :containerClass="'pagination'" v-model="syncCurrentPage" :page-count="10" :page-range="10" :margin-pages="1" :click-handler="clickCallback" :prev-text="'Previous'" :next-text="'Next'" style="display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;">
+        <template slot="page" slot-scope="props"> 
+            <li v-for="(page, index) in props.pages" :key="index" :class="{ 'active': props.isActive(page), 'disabled': props.isDisabled(page)}">
+                <a href="#" @click.prevent="props.pageSelected(page)">{{ page }}</a>
+            </li>
+        </template>
+    </paginate>
+    <div class="clear-fix"></div>
   </div>
 </template>
 
 <script>
 import FreeBoardListForm from '@/components/board/freeBoard/FreeBoardListForm.vue'
 import { mapActions, mapState } from 'vuex'
+import Paginate from 'vuejs-paginate';
 
 const boardModule = 'boardModule'
 
 export default {
-  components: { FreeBoardListForm },
+  components: { FreeBoardListForm, Paginate },
   name: "FreeBoardListPage",
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 5,
+      cache: {},
+      freeBoardList: []
+    }
+  }, 
   computed: {
-      ...mapState(boardModule, ['freeBoards'])
+      ...mapState(boardModule, ['freeBoards']),
+      syncCurrentPage: {
+            get() {
+                return this.currentPage
+            },
+            set(value) {
+                this.currentPage = value
+            }
+      },
+      showFreeBoards: {
+        get() {
+          return this.freeBoardList
+        },
+        set(value) {
+          this.freeBoardList = value;
+        }
+      }
   },
-  mounted () {
-    this.requestFreeBoardListToSpring()
+  async mounted () {
+    console.log("mouted()")
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    console.log(startIndex + "/" + endIndex)
+    const payload = {startIndex: startIndex, endIndex: endIndex}
+    await this.requestFreeBoardListToSpring(payload)
+    this.showFreeBoards = this.freeBoards
+    this.cache[this.currentPage] = this.freeBoards
   },
   methods: {
     ...mapActions(boardModule, [
       'requestFreeBoardListToSpring'
-    ])
+    ]),
+    async clickCallback(pageNumber) {
+      console.log("clickCallback(): " + pageNumber)
+      this.syncCurrentPage = pageNumber
+      const startIndex = (pageNumber - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      
+      if(!this.cache[pageNumber]) {
+        const payload = {startIndex: startIndex, endIndex: endIndex}
+        await this.requestFreeBoardListToSpring(payload)
+        this.showFreeBoards = this.freeBoards
+        this.cache[this.currentPage] = this.freeBoards
+      } else {
+        console.log("cache used!!")
+        this.showFreeBoards = this.cache[pageNumber]
+      }
+        
+    }
   }
 }
 
