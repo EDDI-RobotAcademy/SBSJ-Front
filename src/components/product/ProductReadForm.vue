8ed9f971fb6a1759bf8f33ee3cae20644535f139
Fromredtoviolet <aprilartists@gmail.com>
2023-04-29 22:30:23 +0900

    style: [서예인] 상품 상세 페이지 UI 개선

--- src/components/product/ProductReadForm.vue
+++ src/components/product/ProductReadForm.vue
@@ -10,16 +10,16 @@
                         <div id="carouselDetail" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner" style="height:125px;">
                                 <div class="carousel-item active carousel">
-                                    <img src="https://dummyimage.com/800x400/000/fff&text=Slide+1" class="d-block w-100">
+                                    <img src="@/assets/carousel/carouselImg03.png" class="d-block w-100">
                                 </div>
                                 <div class="carousel-item">
-                                    <img src="https://dummyimage.com/800x400/000/fff&text=Slide+2" class="d-block w-100">
+                                    <img src="@/assets/carousel/carouselImg04.png" class="d-block w-100">
                                 </div>
                             </div>
-                            <div class="carousel-indicators">
+                            <!-- <div class="carousel-indicators">
                                 <button type="button" data-bs-target="#carouselDetail" data-bs-slide-to="0" class="indicator active" aria-current="true" aria-label="Slide 1"></button>
                                 <button type="button" data-bs-target="#carouselDetail" data-bs-slide-to="1" class="indicator" aria-label="Slide 2"></button>
-                            </div>
+                            </div> -->
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselDetail" data-bs-slide="prev" @click="pass">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
@@ -41,16 +41,16 @@
 
                         <div style=" height:15%; width: 100%; display:flex; ">
 
-                            <div style=" width:30%; display:flex; justify-content:center; align-items:center; ">
+                            <!-- <div style=" width:30%; display:flex; justify-content:center; align-items:center; ">
                                 <div style="font-size:xx-large; font-weight: bold; ">10%</div>
-                            </div>
-                            <div style=" width:40%; margin-left:5%;  margin-top:1%;">
-                                <div class="text-decoration-line-through" style=" font-size:medium; display:flex; justify-content:start; align-items:center; opacity: 0.4; color:red; ">{{ new Intl.NumberFormat().format(product.price) }} 원</div>
-                                <div style=" font-size:x-large; color: red; font-weight: bold; display:flex; justify-content:start; align-items:center; ">{{new Intl.NumberFormat().format(Math.floor(product.price * 0.9))}}  원</div>
+                            </div> -->
+                            <div style=" width:40%; margin-left:5%;  margin-top:2%;">
+                                <div style=" font-size:medium; display:flex; justify-content:start; align-items:center;">1개</div>
+                                <div style=" font-size:x-large; font-weight: bold; display:flex; justify-content:start; align-items:center; ">{{new Intl.NumberFormat().format(Math.floor(product.price))}}원</div>
                             </div>
                             <div style=" width:100%; display:flex; ">
-                                <div style="width: 57%; display:flex; justify-content:end; align-items:center; ">
-                                    <div style="width:30%;">
+                                <div style="width: 95%; display:flex; justify-content:end; align-items:center; ">
+                                    <div style="width:15%;">
                                         <div v-if="!this.checkedWish" @click="changeIcon">
                                             <v-icon x-large>mdi-heart-outline</v-icon>
                                         </div>
@@ -64,7 +64,7 @@
                         </div><br>  
 
                         <div style="border-bottom: 2px solid;"></div>
-                        <div style=" width:100%; height:7%;">
+                        <!-- <div style=" width:100%; height:7%;">
                             <div style=" display:flex;  height:100%;  border-bottom: 1px solid #DBDBDB;">
 
                                 <div style="width:30%; display:flex; justify-content:start; align-items:center; font-size:small; font-weight:bold; ">적립금</div>
@@ -73,13 +73,15 @@
                                     <div style=" width:40%; display:flex; justify-content:center; align-items:center; font-size:small; font-weight: bold; ">366p</div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div style="width:100%; height:7%;">
                             <div style="display:flex; height:100%; border-bottom: 1px solid #DBDBDB;">
                                 <div style=" width:30%; height:100%; display:flex; justify-content:start; align-items:center; font-size:small; font-weight:bold; ">배송비</div>
 
-                                <div style=" width:70%; display:flex; justify-content:end; align-items:center; font-size:small; font-weight: bold;">무료배송(우체국택배)</div>
+                                <div style=" width:70%; display:flex; justify-content:end; align-items:center; font-size:small;">
+                                    <span>(5만원 이상 주문시 무료배송)</span> &nbsp;<span style="font-weight:bold;">3,000원</span>
+                                </div>
                             </div>
                         </div>
 
@@ -87,7 +89,9 @@
                             <div style="display:flex; height:100%; border-bottom: 1px solid #DBDBDB;">
                                 <div style="width:30%; height:100%; display:flex; justify-content:start; align-items:center; font-size:small; font-weight:bold; ">간편결제</div>
 
-                                <div style="width:70%; display:flex; justify-content:end; align-items:center; font-size:small; font-weight: bold;"></div>
+                                <div style="width:70%; display:flex; justify-content:end; align-items:center; font-size:small; font-weight: bold;">
+                                    <img src="@/assets/uploadImgs/카카오페이_CI_combination_with_BG.png" style="width: 50px; height: 20px;">
+                                </div>
                             </div>
                         </div>
                         
@@ -106,12 +110,14 @@
                         </div>
                         <div style="width:100%; height:5%; background-color:#F9F9FB; ">
                             <div style=" display:flex; width:100%; height:100%;">
-                                <div style=" width:30%; height:100%; display:flex; justify-content:start; align-items:center; font-size:small; font-weight:bold;">총 결제 금액</div>
+                                <div style=" width:30%; height:100%; display:flex; justify-content:start; align-items:center; font-size:small; font-weight:bold;">상품 금액</div>
                             </div>
                         </div>
                         <div style="width:100%; height:10%; background-color:#F9F9FB; ">
                             <div style=" display:flex; width:100%; height:100%; border-bottom: 1px solid #DBDBDB;">
-                                <div style=" width:30%; height:100%; display:flex; justify-content:start; align-items:center; font-size:small; font-weight:bold;"></div>
+                                <div style=" width:95%; height:100%; display:flex; justify-content:end; align-items:center; font-size:small; font-weight:bold;">
+                                    <div style=" font-size:x-large; font-weight: bold; display:flex; justify-content:end; align-items:center; ">{{new Intl.NumberFormat().format(Math.floor(product.price * this.selectCount))}}원</div>
+                                </div>
                             </div>
                         </div>
                         <div style="width:100%; height:12%; display: flex; justify-content:center; ">
