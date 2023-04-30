02c834d2fdd2f0b90cf309565d93c7168623b0ce
Fromredtoviolet <aprilartists@gmail.com>
2023-04-30 16:48:11 +0900

    style: [서예인] 메인 페이지 UI 수정 [HEALTH-FOOD-WEB-UI-10]

--- src/components/main/MainForm.vue
+++ src/components/main/MainForm.vue
@@ -2,10 +2,11 @@
   <div id="home" class="mt-2">
     <carousel-form/>
     <div class="container">
-      <div style="margin-top: 75px;">
+      <div style="margin-top: 75px; margin-bottom: 25px;">
         <best-product-form/>
       </div>
-      <div style="margin-top: 50px; margin-bottom: 50px;">
+      <v-divider color="black"></v-divider>
+      <div style="margin-top: 25px; margin-bottom: 50px;">
         <recommend-product-form/>
       </div>
     </div>
