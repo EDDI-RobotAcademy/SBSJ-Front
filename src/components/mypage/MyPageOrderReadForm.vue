64fb3de86b5d54234fb6b61b3cef15f6584c5052
Fromredtoviolet <aprilartists@gmail.com>
2023-05-02 03:32:35 +0900

    style: [서예인] 주문 상세 보기 다이얼로그의 스타일 수정 [HEALTH-FOOD-WEB-UI-88]

--- src/components/mypage/MyPageOrderReadForm.vue
+++ src/components/mypage/MyPageOrderReadForm.vue
@@ -2,45 +2,51 @@
     <v-layout>
         <v-dialog v-model="dialog" persistent max-width="800">
             <template v-slot:activator="{on}">
-                <v-btn class="w-10 me-2" v-on="on">상세 보기</v-btn>
+                <v-btn class="w-10 me-2" outlined color="#692498" v-on="on">상세 보기</v-btn>
             </template>
 
-            <v-form @submit.prevent="onSubmit" ref="form">
+            <v-form ref="form">
                 <v-card class="rounded-xl p-3">
                     <div class="d-flex justify-end">
                         <v-icon class="mt-6 me-6 mb-0" @click="btnCancel()">mdi-close</v-icon>
                     </div>
-                    <v-card-title class="headline d-flex justify-center">
+                    <v-card-title class="h4 d-flex justify-center">
                         주문 상세 내역
                     </v-card-title>
                     <br>
                     <v-card-text class="order-read-main mb-5">
-                        <div class="mb-1">주문번호: {{ completeOrder.orderNo }}</div>
-                        <div class="mb-1">주문일시: {{ completeOrder.orderDate | formatDate }}</div>
+                        <div class="mb-1"><strong>주문번호</strong> | {{ completeOrder.orderNo }}</div>
+                        <div class="mb-1"><strong>주문일시</strong> | {{ completeOrder.orderDate | formatDate }}</div>
                         <v-divider color="black"></v-divider>
-                        <div class="mb-1">수령인: {{ completeOrder.recipientName }}</div>
-                        <div class="mb-1">연락처: {{ completeOrder.phoneNumber }}</div>
-                        <div class="mb-1">배송지: {{ completeOrder.road }} {{ completeOrder.addressDetail }}</div>
-                        <div class="mb-1">배송 시 요청사항: {{ completeOrder.selectedDeliveryReq }}</div>
+                        <div class="mb-1"><strong>수령인</strong> | {{ completeOrder.recipientName }}</div>
+                        <div class="mb-1"><strong>연락처</strong> | {{ completeOrder.phoneNumber }}</div>
+                        <div class="mb-1"><strong>배송지</strong> | {{ completeOrder.road }} {{ completeOrder.addressDetail }}</div>
+                        <div class="mb-1"><strong>배송 시 요청사항</strong> | {{ completeOrder.selectedDeliveryReq }}</div>
                         <v-divider color="black"></v-divider>
-                        <div class="mb-1"> 주문상품
-                            <v-card class="mb-1" outlined v-for="(item, index) in completeOrder.orderItemList" :key="index">
-                                <v-img
-                                    :src="require(`@/assets/productImgs/${item.thumbnail}`)"
-                                    aspect-ratio="1"
-                                    max-width="100"
-                                    max-height="100"
-                                    contain
-                                />
-                                <div class="mb-1">상품명: {{ item.productName }}</div>
-                                <div class="mb-1">수량: {{ item.orderItemCount }}</div>
-                            </v-card>
-                            합계금액: {{ new Intl.NumberFormat().format(completeOrder.orderItemList[0].orderItemPrice) }}원
+                        <div class="mb-1"> <strong>주문상품</strong>
+                            <div class="mb-2 p-3" outlined v-for="(item, index) in completeOrder.orderItemList" :key="index">
+                                <v-row>
+                                    <v-col cols="2">
+                                        <v-img
+                                            :src="require(`@/assets/productImgs/${item.thumbnail}`)"
+                                            aspect-ratio="1"
+                                            max-width="100"
+                                            max-height="100"
+                                            contain
+                                        />
+                                    </v-col>
+                                    <v-col>
+                                        <div class="mb-1">상품명: {{ item.productName }}</div>
+                                        <div class="mb-1">수량: {{ item.orderItemCount }}개</div>
+                                    </v-col>
+                                </v-row>
+                            </div>
+                            <strong>합계금액</strong> | {{ new Intl.NumberFormat().format(completeOrder.orderItemList[0].orderItemPrice) }}원
                         </div>
                         <v-divider color="black"></v-divider>
-                        <div class="mb-1">결제번호: {{ completeOrder.merchant_uid }}</div>
-                        <div class="mb-1">결제일시: {{ completeOrder.regData | formatDate }}</div>
-                        <div class="mb-1">결제금액: {{ new Intl.NumberFormat().format(completeOrder.amount) }}원</div>
+                        <div class="mb-1"><strong>결제번호</strong> | {{ completeOrder.merchant_uid }}</div>
+                        <div class="mb-1"><strong>결제일시</strong> | {{ completeOrder.regData | formatDate }}</div>
+                        <div class="mb-1"><strong>결제총액</strong> | {{ new Intl.NumberFormat().format(completeOrder.amount) }}원</div>
                     </v-card-text>
                 </v-card>
             </v-form>
