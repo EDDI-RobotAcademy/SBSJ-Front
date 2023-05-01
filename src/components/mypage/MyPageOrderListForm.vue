98dacc0fed8399cfb82c847eaa27e874418e9982
Fromredtoviolet <aprilartists@gmail.com>
2023-05-02 03:31:53 +0900

    style: [서예인] 마이페이지 주문관리 탭의 스타일 수정 [HEALTH-FOOD-WEB-UI-87]

--- src/components/mypage/MyPageOrderListForm.vue
+++ src/components/mypage/MyPageOrderListForm.vue
@@ -1,21 +1,57 @@
 <template>
-    <div class="mt-10 mx-5">
+    <div class="mt-10 mx-5 grey lighten-4 p-5">
         <div v-if="!completeOrderList || (Array.isArray(completeOrderList) && completeOrderList.length === 0)">
-            <div class="d-flex justify-center">
-                <h1 class="mt-10">아무것도 없음</h1>
-            </div>
+            <v-card outlined flat height="300">
+                <div class="d-flex justify-center align-center h2 empty-msg">
+                    주문 내역이 존재하지 않습니다.
+                </div>
+            </v-card>
         </div>
         <div v-else>
-            <v-card v-for="completeOrder in completeOrderList" :key="completeOrder.orderId" class="mb-5 rounded-xl">
-                <v-card-text>
-                    <div class="mb-1">주문번호: {{ completeOrder.orderNo }}</div>
-                    <div class="mb-1">주문일시: {{ completeOrder.orderDate | formatDate }}</div>
-                    <div class="mb-1">
-                        주문상품: {{ completeOrder.orderTotalCount === 1 ? completeOrder.orderItemList[0].productName : `${completeOrder.orderItemList[0].productName} 외 ${completeOrder.orderTotalCount - 1}건` }}
-                    </div>
-                    <div class="mb-1">결제금액: {{ new Intl.NumberFormat().format(completeOrder.amount) }}원</div>
-                </v-card-text>
-                <v-card-actions class="ms-2 pb-5 pt-0">
+            <v-card v-for="completeOrder in completeOrderList" :key="completeOrder.orderId" class="m-5 p-5 rounded-xl" flat outlined>
+                <div>
+                    <v-chip class="ms-3">결제완료</v-chip>
+                    <v-row>
+                        <v-col cols="8">
+                            <v-card-text>
+                                <v-row>
+                                    <v-col class="mt-1">
+                                        <div><strong>주문번호</strong></div>
+                                        <div>{{ completeOrder.orderNo }}</div>
+                                    </v-col>
+                                    <v-col>
+                                        <div><strong>주문일시</strong></div>
+                                        <div>{{ completeOrder.orderDate | formatDate }}</div>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col>
+                                        <div><strong>주문상품</strong></div>
+                                        <div class="h4">
+                                            {{ completeOrder.orderTotalCount === 1 ? completeOrder.orderItemList[0].productName : `${completeOrder.orderItemList[0].productName} 외 ${completeOrder.orderTotalCount - 1}건` }}
+                                        </div>
+                                    </v-col>
+                                    <v-col>
+                                        <div><strong>결제금액</strong></div>
+                                        <div class="h4">
+                                            {{ new Intl.NumberFormat().format(completeOrder.amount) }}원
+                                        </div>
+                                    </v-col>
+                                </v-row>
+                            </v-card-text>
+                        </v-col>
+                        <v-col>
+                            <v-img
+                                :src="require(`@/assets/productImgs/${completeOrder.orderItemList[0].thumbnail}`)"
+                                aspect-ratio="1"
+                                max-width="200"
+                                max-height="200"
+                                contain
+                            />
+                        </v-col>
+                    </v-row>
+                </div>
+                <v-card-actions class="ms-2 pb-5 pt-0 justify-end">
                     <my-page-order-read-form :completeOrder="completeOrder"/>
                 </v-card-actions>
             </v-card>
@@ -57,6 +93,10 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+    .empty-msg {
+        margin-top: 130px;
+    }
 
 </style>
