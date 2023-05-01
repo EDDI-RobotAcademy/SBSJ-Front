0db22c5945a26f3050ef512873b7ba37b1b684ee
Fromredtoviolet <aprilartists@gmail.com>
2023-05-02 03:28:56 +0900

    style: [서예인] 마이페이지의 배송지 관리 탭 스타일 수정 [HEALTH-FOOD-WEB-UI-48]

--- src/components/mypage/MyPageDeliveryListForm.vue
+++ src/components/mypage/MyPageDeliveryListForm.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="mt-10 mx-5">
+    <div class="mt-10 mx-5 grey lighten-4 p-5">
         <div v-if="!localDeliveryList || (Array.isArray(localDeliveryList) && localDeliveryList.length === 0)">
             <div class="d-flex justify-end mb-5">
                 <my-page-register-address-form @submit="onSubmit"/>
             </div>
-            <div class="d-flex justify-center">
-                <h1 class="mt-10">아무것도 없음</h1>
-            </div>
+            <v-card outlined flat height="300">
+                <div class="d-flex justify-center align-center h2 empty-msg">
+                    등록된 배송지가 없습니다.
+                </div>
+            </v-card>
         </div>
         <div v-else>
-            <div class="d-flex justify-end mb-5">
+            <div class="d-flex justify-end mb-5 me-8">
                 <my-page-register-address-form @submit="onSubmit"/>
             </div>
-            <v-card v-for="delivery in localDeliveryList" :key="delivery.addressId" class="mb-5 rounded-xl">
+            <v-card v-for="delivery in localDeliveryList" :key="delivery.addressId" class="mb-5 mx-5 rounded-xl p-4" flat outlined>
+                
                 <v-card-title style="font-size: 20px;">
-                    <strong>[{{ delivery.addressType }}]&nbsp;{{ delivery.addressName }}</strong> <br>
+                    <strong>[{{ delivery.addressType }}]&nbsp;{{ delivery.addressName }}</strong>
                 </v-card-title>
                 <div v-if="delivery.defaultAddress" style="font-size: 15px;">
-                    <span class="ms-4 p-1 border border-1 border-secondary rounded-pill w-10">기본배송지</span>
+                    <span class="ms-4 px-2 py-1 border border-1 border-secondary rounded-pill w-10">기본배송지</span>
                 </div>
                 
                 <v-card-text style="font-size: 18px;">
-                    <div class="mb-1">수령인: {{ delivery.recipientName }}</div>
-                    <div class="mb-1">{{ delivery.road }} {{ delivery.addressDetail }}</div>
-                    <div class="mb-1">{{ delivery.zipcode }}</div>
-                    <div class="mb-1">{{ delivery.phoneNumber }}</div>
+                    <div class="mb-1">수령인 : {{ delivery.recipientName }}</div>
+                    <div class="mb-1">연락처 : {{ delivery.phoneNumber }}</div>
+                    <div class="mb-1">주소지 : {{ delivery.road }} {{ delivery.addressDetail }} ({{ delivery.zipcode }})</div>
                 </v-card-text>
                 <v-card-actions class="ms-2 pb-5 pt-0">
                     <my-page-delivery-modify-form :delivery="delivery"/>
@@ -100,5 +102,9 @@ export default {
 </script>
 
 <style scoped>
+
+    .empty-msg {
+        margin-top: 130px;
+    }
 
 </style>
