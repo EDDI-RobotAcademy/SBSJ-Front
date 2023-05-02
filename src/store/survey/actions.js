9adb19a9f70319357de4209effed8273ef4a2f60
vceqiv09 <vceqiv@gmail.com>
2023-05-03 08:45:22 +0900

    feat: [윤영현] '맞춤 영양제 추천' 결과를 백으로 결과 저장 요청을 보내는 기능 추가 [HEALTH-FOOD-WEB-UI-107]

--- src/store/survey/actions.js
+++ src/store/survey/actions.js
@@ -1,5 +1,31 @@
 import {
+    SET_COMMON_SURVEY_RESULT,
+    SET_VISCERA_SURVEY_RESULT,
+    SET_LIFE_SURVEY_RESULT
 } from "./mutation-types";
+
+import mainRequest from '@/api/mainRequest';
+
 export default {
 
+    setSurveyResult({ commit }, payload) {
+        console.log(payload);
+        commit(SET_COMMON_SURVEY_RESULT, payload[0]);
+        commit(SET_VISCERA_SURVEY_RESULT, payload[1]);
+        commit(SET_LIFE_SURVEY_RESULT, payload[2]);
+    },
+
+    // 설문조사 결과 저장
+    reqRegisterSurveyResultToSpring({}, payload) {
+        return mainRequest.post('/survey/register', payload)
+        .then((res) => {
+            if(res.data) {
+                alert("설문조사 등록 성공");
+            }
+        })
+        .catch((error) => {
+            alert("설문조사 등록 에러가 발생했습니다: " + error.message);
+        })
+    },
+    
 }
--- src/store/survey/surveyStates.js
+++ src/store/survey/surveyStates.js
@@ -1,3 +1,7 @@
 export default {
 
+    commonSurveyResult: {},
+    visceraSurveyResult: {},
+    lifeSurveyResult: {}
+
 }
