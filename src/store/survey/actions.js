ba194bcef1f05e311b0d345f8bc782323c3d0f63
vceqiv09 <vceqiv@gmail.com>
2023-05-03 08:46:09 +0900

    feat: [윤영현] 로그인 한 회원이 마이페이지의 ‘맞춤 영양제 추천’ 탭을 누르면 자신의 맞춤 영양제 추천 결과를 보여주는 UI [HEALTH-FOOD-WEB-UI-108]

--- src/store/survey/actions.js
+++ src/store/survey/actions.js
@@ -15,6 +15,32 @@ export default {
         commit(SET_LIFE_SURVEY_RESULT, payload[2]);
     },
 
+    // 설문조사 가져오기
+    reqReadSurveyResultToSpring({ dispatch }, memberId) {
+        return mainRequest.post('/survey/read', { memberId })
+        .then((res) => {
+            let response = res.data
+            if(response == '') {
+                return null;
+            } else {
+                let commonSurveyResult = {
+                    "username": response.username, 
+                    "gender": response.gender,
+                    "age": response.age,
+                    "height": response.height,
+                    "weight": response.weight
+                }
+                let visceraSurveyResult = { "viscera" : response.viscera }
+                let lifeSurveyResult = { "life" : response.life }
+
+                dispatch('setSurveyResult', [commonSurveyResult, visceraSurveyResult, lifeSurveyResult]);
+            }
+        })
+        .catch((error) => {
+            alert("설문조사 읽기 에러가 발생했습니다: " + error.message);
+        })
+    },
+
     // 설문조사 결과 저장
     reqRegisterSurveyResultToSpring({}, payload) {
         return mainRequest.post('/survey/register', payload)
