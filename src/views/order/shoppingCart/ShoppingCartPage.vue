2bf3c906207992bfe1ef1cc8bb91e85c26bcadbe
vceqiv09 <vceqiv@gmail.com>
2023-05-03 08:33:52 +0900

    add: [윤영현] DB 조회 함수 ShoppingCartForm 으로 이동 [HEALTH-FOOD-WEB-UI-27]

--- src/views/order/shoppingCart/ShoppingCartPage.vue
+++ src/views/order/shoppingCart/ShoppingCartPage.vue
@@ -5,33 +5,12 @@
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
 import ShoppingCartForm from '@/components/order/ShoppingCartForm.vue'
 
-const orderModule = 'orderModule'
-const accountModule = 'accountModule'
 
 export default {
     name: "ShoppingCartPage",
-    components: { ShoppingCartForm },
-    computed: {
-        ...mapState(accountModule, ['isAuthenticated']),
-    },
-    methods:{
-        ...mapActions(orderModule, [
-            'reqCartItemListToSpring',
-        ]),
-    },
-    async created () {
-        console.log("CartPage created()");
-        //if(this.isAuthenticated === true) {
-            console.log("CartPage created2()");
-            let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-            console.log("CartPage created3() userInfo: " + userInfo.token);
-            
-            await this.reqCartItemListToSpring(userInfo);
-       // }
-    },
+    components: { ShoppingCartForm }
 }
 </script>
 
